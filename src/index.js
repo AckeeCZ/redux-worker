186ae4abebe81b106a256cc056e39159e650c46e
Jiří Čermák <jiri.cermak@ackee.cz>
2019-01-25 15:20:50 +0100

    Export MainThread object as public low-level API for worker context

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 // common exports for main and worker context
 export { default as uniqueId } from './utils/uniqueId';
+export { ALL_MESSAGES } from './modules/worker-adapter';
--- src/index.worker.js
+++ src/index.worker.js
@@ -1,2 +1,3 @@
 export { configureStoreWorker, registerSelector } from './modules/store-worker/worker';
 export { executeInWindow } from './modules/window-bridge/worker';
+export { MainThread } from './modules/worker-adapter/worker';
